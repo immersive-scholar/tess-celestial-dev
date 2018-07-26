4ceccecf107359caf482a7e9ab4201f14a50800c
WaltGurley <walt.gurley@gmail.com>
2018-07-26 15:03:36 -0400

    popup loads with images

--- src/index.js
+++ src/index.js
@@ -13,27 +13,27 @@ const imgFolder = `test-img`
 const matrix = document.querySelector('.matrix')
 
 // This function takes in an image source path and a title to create an img element with alt-text. It returns the created img element.
-const createImgElements = function (imgSrc, title) {
+const createImgElement = function (imgSrc, title) {
   const img = document.createElement('img')
   img.src = require(`./${imgFolder}/${imgSrc}`)
   img.alt = title
-  img.className = 'matrix-item'
-  img.dataset.title = title
+  img.dataset.id = title.split(' ').join('')
   return img
 }
 
 // An array containing the image elements of the matrix
-const matrixImgs = testImgData.map(d => createImgElements(d.imgName, d.title))
+const matrixImgs = testImgData.map(d => createImgElement(d.imgName, d.title))
 
 // Append matrix images to the matrix container
 matrixImgs.forEach(function (img) {
+  img.className = 'matrix-item'
   matrix.appendChild(img)
 })
 
-// This function takes in an array of objects with key values specified in 'data.csv' and returns an object with keys of 'title' and values of relative popup assets
+// This function takes in an array of objects with key names specified in 'data.csv' and returns an object with keys of 'title' and values of relative popup assets
 const preparePopupAssets = function (assetsObject, value) {
-  assetsObject[value.title] = {
-    title: value.title,
+  assetsObject[value.title.split(' ').join('')] = {
+    id: value.title,
     text: value.storyText,
     storyMedia: [
       value.storyMedia1,
@@ -47,17 +47,31 @@ const preparePopupAssets = function (assetsObject, value) {
 }
 
 const popupAssets = testImgData.reduce(preparePopupAssets, {})
-console.log(popupAssets)
 
-const showPopup = function (e) {
-  e.stopPropagation()
-  console.log('popup!')
+const showPopup = function (event) {
+  const storyId = this.dataset.id
+  const story = popupAssets[storyId]
+
+  // Set source of main img to img selected from matrix
+  document.querySelector('#primary-popup-img').src = this.src
+
+  const secondaryImgs = document.querySelectorAll('.secondary-img')
+  console.log(story)
+  story.storyMedia
+    .filter(d => d !== '')
+    .forEach(function (mediaItem, i) {
+      secondaryImgs[i].classList.add('visible')
+      secondaryImgs[i].src = require(`./${imgFolder}/${storyId}/${mediaItem}`)
+    })
+
   const storyPopup = document.querySelector('.matrix-popup')
   storyPopup.classList.add('visible')
 
+  event.stopPropagation()
   const removePopup = function () {
     console.log('popup!')
     storyPopup.classList.remove('visible')
+    secondaryImgs.forEach(d => d.classList.remove('visible'))
     document.querySelector('body').removeEventListener('click', removePopup)
   }
   document.querySelector('body').addEventListener('click', removePopup)
