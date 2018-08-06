73c0e720a1bf7ae9c36f96dced24261f97076ba5
WaltGurley <walt.gurley@gmail.com>
2018-08-06 17:04:20 -0400

    zoom in complete

--- src/matrixZoomAnimation.js
+++ src/matrixZoomAnimation.js
@@ -7,8 +7,20 @@ function matrixZoomAnimation (container, element) {
     .split(' ').length
   const rowShift = rows / 2 - 0.5
 
-  const elementColPos = Math.floor(+element.dataset.pos / columns)
-  console.log(-columnShift, -rowShift, elementColPos)
+  const elementRowPos = Math.floor(+element.dataset.pos / columns)
+  const elementColPos = +element.dataset.pos - elementRowPos * columns
+
+  const xTranslate = (columnShift - elementColPos) / columns
+  const yTranslate = (rowShift - elementRowPos) / rows
+
+  const containerAnimation = container.animate([
+    {transform: 'scale(1) translate(0)'},
+    {transform: `scale(12) translateX(calc(${xTranslate} * 100%)) translateY(calc(${yTranslate} * 100%))`}
+  ], 3000)
+  containerAnimation.addEventListener('finish', function () {
+    container.style.transform = `scale(12) translateX(calc(${xTranslate} * 100%)) translateY(calc(${yTranslate} * 100%))`
+  })
+  console.log(-columnShift, -rowShift, xTranslate * 100, yTranslate * 100)
 }
 
 export default matrixZoomAnimation
