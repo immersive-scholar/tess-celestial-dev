7f5453d72b019f892608a6134e261a11692ec0d8
WaltGurley <walt.gurley@gmail.com>
2018-08-15 17:13:07 -0400

    blur on zoom, cleaned up small imgs

--- src/matrixZoomAnimation.js
+++ src/matrixZoomAnimation.js
@@ -15,32 +15,37 @@ function matrixZoomAnimation (container, element, popup, direction) {
 
   const animationDirection = direction === 'zoom-in' ? [
     {
-      transform: 'scale(1) translate3d(0,0,0)'
-      // opacity: '1'
+      transform: 'scale(1) translate(0)',
+      opacity: '1',
+      filter: 'blur(0)'
     },
     {
-      transform: `scale(12) translateX(calc(${xTranslate} * 100%)) translateY(calc(${yTranslate} * 100%)) translateZ(0)`
-      // opacity: '0.5'
+      transform: `scale(12) translateX(calc(${xTranslate} * 100%)) translateY(calc(${yTranslate} * 100%))`,
+      opacity: '0.5',
+      filter: 'blur(2px)'
     }
   ] : direction === 'zoom-out' ? [
     {
-      transform: `scale(12) translateX(calc(${xTranslate} * 100%)) translateY(calc(${yTranslate} * 100%)) translateZ(0)`
-      // opacity: '0.5'
+      transform: `scale(12) translateX(calc(${xTranslate} * 100%)) translateY(calc(${yTranslate} * 100%))`,
+      opacity: '0.5',
+      filter: 'blur(2px)'
     },
     {
-      transform: 'scale(1) translate3d(0,0,0)'
-      // opacity: '1'
+      transform: 'scale(1) translate(0)',
+      opacity: '1',
+      filter: 'blur(0)'
     }
   ] : null
 
   const containerAnimation = container.animate(animationDirection, {
     easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
-    duration: 6000
+    duration: 8000
   })
 
   containerAnimation.addEventListener('finish', function () {
     container.style.transform = animationDirection[1].transform
     container.style.opacity = animationDirection[1].opacity
+    container.style.filter = animationDirection[1].filter
     if (direction === 'zoom-in') {
       popup.classList.add('visible')
     } else if (direction === 'zoom-out') {
